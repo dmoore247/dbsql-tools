0c231828e111aeac57328fa46a46748898023d26
anhhchu <anhhchu>
2024-08-01 23:57:26 +0000

    First commit

---
+++ main.py
@@ -0,0 +1,144 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # Purpose
+# MAGIC
+# MAGIC This tool is designed to automatically create a dashboard that helps identify high usage for Interactive clusters still being used for SQL workloads, that would be better served via the specialized DBSQL Warehouse endpoints.
+# MAGIC
+# MAGIC ![Move to the right](https://github.com/user-attachments/assets/6978e97a-d76e-4949-bcbf-ff7c45b04276)
+# MAGIC
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Instructions
+# MAGIC
+# MAGIC 1. Execute all cells in the "Set Up" section.
+# MAGIC 2. Verify if the system schemas (access, compute, billing) are enabled in your workspace:
+# MAGIC    - If enabled, proceed to Step 2: Create Dashboard.
+# MAGIC    - If not enabled:
+# MAGIC      - Proceed to Step 3: Enable System Tables (ensure an account admin is available to perform this step).
+# MAGIC      - Return to Step 2.
+# MAGIC
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Step 1: Set Up
+
+# COMMAND ----------
+
+import pandas as pd
+import logging
+from databricks.sdk import WorkspaceClient
+import os
+import requests
+import re
+import json
+
+# COMMAND ----------
+
+# MAGIC %run ./utils/lakeview_dash_manager
+
+# COMMAND ----------
+
+HOSTNAME = spark.conf.get('spark.databricks.workspaceUrl')
+TOKEN = dbutils.notebook.entry_point.getDbutils().notebook().getContext().apiToken().get()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Step 2: Create Dashboard
+# MAGIC
+# MAGIC * The Lakeview dashboard is generated using the template: `./utils/Better SQL for Customers.lvdash.json`.
+# MAGIC * This template is stored in the user's workspace location.
+# MAGIC * All dashboard assets for each user are also saved in their respective workspace locations.
+# MAGIC
+# MAGIC A link to the dashboard draft will be provided, and direct you to the dashboard. Click on the top to publish the dashboard and share it with other users.
+# MAGIC
+# MAGIC
+
+# COMMAND ----------
+
+user_name = dbutils.notebook.entry_point.getDbutils().notebook().getContext().userName().get()
+lv_workspace_path = f"/Users/{user_name}"
+lv_dashboard_name = "Better SQL for Customers"
+
+# COMMAND ----------
+
+# DBTITLE 1,Generate dashboard
+lv_api = LakeviewDashManager(host=HOSTNAME, token=TOKEN)
+lv_api.load_dash_local("./utils/Better SQL for Customers.lvdash.json")
+dashboard_link = lv_api.import_dash(path=lv_workspace_path, dashboard_name=lv_dashboard_name)
+print(f"Dashboard is ready at: {dashboard_link}")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Step 3: Enable System Tables (Optional)
+# MAGIC
+# MAGIC Execute all cells in this section to enable system schemas (compute, access, billing) if they are not already enabled in your workspace.
+# MAGIC
+# MAGIC **Note:** Only an account admin can run these cells.
+# MAGIC
+# MAGIC Account admin will be prompted to input the metastore_id under Catalog Explorer setting
+# MAGIC
+# MAGIC The account admin will also be prompted to provide a list of email addresses to grant SELECT access to these system tables.
+# MAGIC
+# MAGIC ---
+# MAGIC
+# MAGIC Since system tables are governed by Unity Catalog, you must have at least one Unity Catalog-enabled workspace in your account to enable and access system tables. System tables include data from all workspaces in your account but can only be accessed from a Unity Catalog-enabled workspace. (System tables are not available in AWS GovCloud regions.)
+# MAGIC
+# MAGIC The following system tables are required to run the dashboard:
+# MAGIC   * `system.access.audit`
+# MAGIC   * `system.compute.clusters`
+# MAGIC   * `system.billing.usage`
+# MAGIC
+# MAGIC We need to enable three schemas: access, compute, and billing in the system catalog. The billing schema is enabled by default; other schemas must be enabled manually.
+# MAGIC
+# MAGIC ---
+# MAGIC
+
+# COMMAND ----------
+
+required_system_tables = ["system.access.audit", "system.compute.clusters", "system.billing.usage"]
+required_system_schemas = ["compute", "access"]
+
+# COMMAND ----------
+
+# DBTITLE 1,Input metastore_id
+metastore_id = input("Provide metastore_id for this workspace: ")
+
+# COMMAND ----------
+
+# DBTITLE 1,Enable system tables
+
+
+def enable_schema(schema_name):
+    endpoint = f"https://{HOSTNAME}/api/2.1/unity-catalog/metastores/{metastore_id}/systemschemas/{schema_name}"
+
+    headers = {
+        "Authorization": f"Bearer {TOKEN}",
+        "Content-Type": "application/json"
+    }
+
+    response = requests.put(endpoint, headers=headers)
+    if response.status_code == 200:
+        print(f"System schema {schema_name} enabled successfully.")
+    else:
+        print(f"Failed to enable system schema {schema_name}. Status code: {response.status_code}, Message: {response.text}")
+
+for schema_name in required_system_schemas:
+    enable_schema(schema_name)
+
+# COMMAND ----------
+
+# DBTITLE 1,Grant access to users
+users = input("Provide list of users to grant access to system tables, separating by commas: ")
+
+# COMMAND ----------
+
+users_list = [user.strip() for user in users.split(",")]
+
+for user in users_list:
+  for table in required_system_tables:
+    spark.sql(f"GRANT SELECT ON TABLE {table} TO `{user}`")
---
+++ utils/lakeview_dash_manager.py
@@ -0,0 +1,134 @@
+# Databricks notebook source
+import base64
+import json
+import re
+
+import requests
+
+
+class LakeviewDashManager:
+    """Lakeview Dashboard API class
+    """
+    def __init__(self, host, token):
+        self.host = host
+        self.token = token
+        self.url_base = f"https://{self.host}/api/2.0/workspace"
+        self.headers = {"Authorization": f"Bearer {self.token}"}
+        self.lakeview_json = None
+
+    def list_content(self, path):
+        """List out the content from the path of a workspace
+
+        Args:
+            path: workspace path to list content from
+
+        Returns:
+            a json object with the content of the workspace path
+        """
+        url = f"{self.url_base}/list"
+        data = {"path": path}
+        response = requests.get(url, headers=self.headers, json=data)
+        return response.json()
+
+    def export_dash(self, path, dashboard_name):
+        """export dashboard with given workspace path and dashboard name
+
+        Args:
+            path): workspace path to export lakeview dashboard from
+            dashboard_name: lakeview dashboard name to export
+        """
+        url = f"{self.url_base}/export"
+        path = f"{path}/{dashboard_name}.lvdash.json"
+        data = {"path": path, "direct_download": True}
+        response = requests.get(url, headers=self.headers, json=data)
+        self.lakeview_json = json.loads(response.text)
+        return
+
+    def import_dash(self, path, dashboard_name):
+        """_summary_
+
+        Args:
+            path: workspace path to import lakeview dashboard to
+            dashboard_name: lakeview dashboard name to write to
+
+        Returns:
+            api resonse (expect to be 200 for success)
+        """
+        url = f"{self.url_base}/import"
+        path_full = f"{path}/{dashboard_name}.lvdash.json"
+        data = {
+            "content": self.basee64_encode(json.dumps(self.lakeview_json)),
+            "path": path_full,
+            "overwrite": True,
+            "format": "AUTO",
+        }
+        response = requests.post(url, headers=self.headers, json=data)
+        if response.status_code != 200:
+            raise Exception(f"Unexpected status code: {response.status_code}")
+        return self.get_dashboard_links(path, dashboard_name)
+    
+    def save_dash_local(self, path):
+        """save lakeview dashboard json to local file
+
+        Args:
+            path: path to save the lakeview dashboard to
+        """
+        with open(path, "w") as f:
+            json.dump(self.lakeview_json, f, indent=4)
+        return
+    
+    def load_dash_local(self, path):
+        """load lakeview dashboard template from local file
+
+        Args:
+            path: path to load the lakeview dashboard tempate from
+        """
+        with open(path, "r") as f:
+            self.lakeview_json = json.load(f)
+
+    def set_query_uc(self, catalog_name, schema_name, table_name):
+        """update the catalog_name, schema_name, table_name in the lakeview dashboard json obj
+
+        Args:
+            catalog_name: catalog_name in string
+            schema_name: schema_name in string
+            table_name: metrics table_name in string
+        """
+        for item in self.lakeview_json["datasets"]:
+            item["query"] = re.sub(r"CATALOG_NAME", catalog_name, item["query"])
+            item["query"] = re.sub(r"SCHEMA_NAME", schema_name, item["query"])
+            item["query"] = re.sub(r"TABLE_NAME", table_name, item["query"])
+
+    def get_dashboard_links(self, path, dashboard_name):
+        """Get the dashboard_id based on workspace path of a dashboard
+        http link follows the format of {workspace_host}/sql/dashboardsv3/{dashboard_id}
+        
+        Args:
+            path: workspace path in string
+            dashboard_name: lakeview dashboard name to write to
+
+        Return: 
+            dashboard http links
+        """
+        url = f"{self.url_base}/get-status"
+        path_full = f"{path}/{dashboard_name}.lvdash.json"
+        data = {
+            "path": path_full,
+        }
+        response = requests.get(url, headers=self.headers, json=data)
+        dashboard_id = response.json().get('resource_id')
+        return f"https://{self.host}/sql/dashboardsv3/{dashboard_id}"
+
+    @staticmethod
+    def basee64_encode(x):
+        """base64 encode a string
+
+        Args:
+            x: string to encode
+
+        Returns:
+            endcoded string
+        """
+        encoded_bytes = base64.b64encode(x.encode("utf-8"))
+        encoded_string = encoded_bytes.decode("utf-8")
+        return encoded_string
